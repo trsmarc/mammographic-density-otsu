7ca541e67540d0ba297ed3e79b43e5e6585c91a9
Marktrs <marktrs69@gmail.com>
2017-06-14 14:12:40 +0900

    Declare image file

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import cv2
 import windowmanager as window
 import breast_segmentation as bs
 
-img = cv2.imread('image/MLO_test.bmp')
+origin_img = 'image/MLO_test.bmp'
+
+img = cv2.imread(origin_img)
 img2 = bs.breast_display(img)
 
 window.create_window(img, img2)
